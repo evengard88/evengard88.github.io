84ee71f2631e2961d2e1aba3a188bae26d5d1ce5
evengard88 <evengard88@users.noreply.github.com>
2026-02-23 18:36:28 +0000

    deploy: evengard88/SnakeMeasurer@dedad320abf9c88315d08ff39e4418d6209ea08f

--- app/sw.js
+++ app/sw.js
@@ -2,7 +2,7 @@
 // Cache-first strategy for WASM/JS assets, network-first for HTML
 // Auto-updates when new version is deployed
 
-var CACHE_VERSION = 'v4-bunny-storage';
+var CACHE_VERSION = 'v6-langs';
 var CACHE_NAME = 'snake-measurer-' + CACHE_VERSION;
 
 // Assets cached on install (shell)
